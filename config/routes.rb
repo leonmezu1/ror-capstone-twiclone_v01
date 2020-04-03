faea73c5e07d9a3e853cc140fa4000af22609b47
Leonardo Mezu <52045562+leonmezu1@users.noreply.github.com>
2020-04-03 16:16:24 -0500

    Added devise integration for model views and all

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  devise_for :users
+  get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
