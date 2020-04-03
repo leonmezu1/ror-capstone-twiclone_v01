44d2d942e097a1305d96096dd5e464d7795a7d11
Leonardo Mezu <52045562+leonmezu1@users.noreply.github.com>
2020-04-03 16:34:44 -0500

    added basic route and authenticate_user! restriction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,22 @@
 # frozen_string_literal: true
 
+# Application controller definitions
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:username, :full_name,
+               :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:username, :full_name,
+               :email, :password, :password_confirmation)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  get 'welcome/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'welcome#index'
 end
