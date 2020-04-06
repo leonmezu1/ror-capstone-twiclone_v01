817f52c79cc6f31d2d243b5dbc6e9b2a70f4cf15
Leonardo Mezu <52045562+leonmezu1@users.noreply.github.com>
2020-04-06 02:33:31 -0500

    updated chirp model

--- app/models/chirp.rb
+++ app/models/chirp.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+# Chirps
 class Chirp < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true, length: { minimum: 5, maximum: 255 }
+  scope :ordered_by_most_recent, -> { order(created_at: :desc) }
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 end
