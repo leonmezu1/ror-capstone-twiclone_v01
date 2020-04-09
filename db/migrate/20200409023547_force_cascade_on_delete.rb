3d2d4427792f0b87c05b46876d66a490efdabc18
Leonardo Mezu <52045562+leonmezu1@users.noreply.github.com>
2020-04-08 21:39:43 -0500

    Force cascade deletion on schema

---
+++ db/migrate/20200409023547_force_cascade_on_delete.rb
@@ -0,0 +1,8 @@
+class ForceCascadeOnDelete < ActiveRecord::Migration[6.0]
+  def change
+    remove_foreign_key :comments, :chirps
+    add_foreign_key :comments, :chirps, on_delete: :cascade
+    remove_foreign_key :likes, :chirps
+    add_foreign_key :likes, :chirps, on_delete: :cascade
+  end
+end
